156deaa41ca1df674c41d21ad4a4bec004695b53
Hamid1689 <halilillo603@gmail.com>
2026-07-22 18:24:09 +0600

    fix firebase init

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDiWBt-tjJ-dsmFsshbZ97To9pgWXmGHMk",
   authDomain: "halil-ice.firebaseapp.com",
@@ -8,3 +11,6 @@ const firebaseConfig = {
   appId: "1:896191058330:web:1274d29592bc8233416a23",
   measurementId: "G-PHMF2B3FP5"
 };
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
